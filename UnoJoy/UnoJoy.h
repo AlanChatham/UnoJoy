8a1efb074e7d0a19f706cf536cbe080732711eab
Alan Chatham <alan.chatham@gmail.com>
2023-12-13 20:34:20 -0800

    Added license info

--- UnoJoy/UnoJoy.h
+++ UnoJoy/UnoJoy.h
@@ -42,6 +42,19 @@
  *      sexyControllerData.triangleOn = 1;   // Marks the triangle button as pressed
  *      sexyControllerData.squareOn = 0;     // Marks the square button as unpressed
  *      sexyControllerData.leftStickX = 90;  // Analog stick values can range from 0 - 255
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef UNOJOY_H
